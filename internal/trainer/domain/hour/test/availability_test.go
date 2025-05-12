8a98542d1510feb1a70f1ac8b8c29bd97b53c8a9
quangbach <buiquangbach27@gmail.com>
2025-05-12 22:28:56 +0700

    adding hour training domain

---
+++ internal/trainer/domain/hour/test/availability_test.go
@@ -0,0 +1,37 @@
+package hour_test
+
+import (
+	"testing"
+
+	"github.com/quangbach27/wild-workout/internal/trainer/domain/hour"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHour_MakeNotAvailable(t *testing.T) {
+	newNotAvailableHour(t)
+}
+
+func TestHour_MakeNotAvailable_with_schudule_training(t *testing.T) {
+	h := newHourWithScheduledTraining(t)
+
+	require.Equal(t, hour.ErrTrainingScheduled, h.MakeNotAvailable())
+}
+
+func newHourWithScheduledTraining(t *testing.T) *hour.Hour {
+	h, err := testHourFactory.NewAvailableHour(validTrainingHour())
+	require.NoError(t, err)
+
+	require.NoError(t, h.ScheduleTraining())
+
+	return h
+}
+
+func newNotAvailableHour(t *testing.T) *hour.Hour {
+	h, err := testHourFactory.NewAvailableHour(validTrainingHour())
+	require.NoError(t, err)
+
+	require.NoError(t, h.MakeNotAvailable())
+	require.False(t, h.IsAvailable())
+
+	return h
+}
